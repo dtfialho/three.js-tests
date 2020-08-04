fedfd73a158016cda96e8788524a8cbe623a8c2e
Diego T. Fialho <diego.tfialho@gmail.com>
2020-08-04 13:07:47 -0300

    camera added

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
-var root = document.querySelector('#root');
-var renderer = new THREE.WebGLRenderer({ canvas: root, antialias: true });
+const root = document.querySelector('#root');
+const renderer = new THREE.WebGLRenderer({ canvas: root, antialias: true });
 
 renderer.setClearColor(0x00ff00);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+const aspect = window.innerWidth / window.innerHeight;
+const camera = new THREE.PerspectiveCamera(35, aspect, 0.1, 3000);
