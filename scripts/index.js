f0ac7ecdf4ddb2dd45b1f524501e2868c127c8a3
Diego T. Fialho <diego.tfialho@gmail.com>
2020-08-04 13:14:56 -0300

    first object added to scene

--- scripts/index.js
+++ scripts/index.js
@@ -8,5 +8,11 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const aspect = window.innerWidth / window.innerHeight;
 const camera = new THREE.PerspectiveCamera(35, aspect, 0.1, 3000);
 const scene = new THREE.Scene();
+const geometry = new THREE.CubeGeometry(100, 100, 100);
+const material = new THREE.MeshBasicMaterial();
+const mesh = new THREE.Mesh(geometry, material);
 
+mesh.position.set(0, 0, -1000);
+
+scene.add(mesh);
 renderer.render(scene, camera);
