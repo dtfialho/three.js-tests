b177fbe70f78d0ab4a794cd23c69779bf10dd931
Diego T. Fialho <diego.tfialho@gmail.com>
2020-08-04 13:09:18 -0300

    first scene added

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,6 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 const aspect = window.innerWidth / window.innerHeight;
 const camera = new THREE.PerspectiveCamera(35, aspect, 0.1, 3000);
+const scene = new THREE.Scene();
+
+renderer.render(scene, camera);
